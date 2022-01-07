cd527ae251aad46b20c1d330178af472cf76a541
Lucas R. Antunes <lucasribeiroantunes2013@gmail.com>
2022-01-06 21:13:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-//https://www.google.com/search?q=c+2d+array+modify+on+function&sxsrf=ALeKk024UN17c_Jqq90fLkXiglDulsHVCw%3A1619325681105&ei=8fKEYLrjBbSe5OUPmraswAk&oq=c+2d+array+modify+on+function&gs_lcp=Cgdnd3Mtd2l6EAM6BwgjELADECc6BwgAEEcQsAM6BggjECcQEzoICAAQBxAeEBM6BggAEB4QEzoICAAQCBAeEBM6BAgAEBM6CggAEAgQBxAeEBM6CggAEAgQDRAeEBM6CAgAEAgQBxAeOgYIABAIEB5Q4NoBWK3pAWDb6wFoBnACeACAAX-IAbkJkgEEMC4xMZgBAKABAaoBB2d3cy13aXrIAQnAAQE&sclient=gws-wiz&ved=0ahUKEwj6n_2fypjwAhU0D7kGHRobC5gQ4dUDCA4&uact=5
 int division(int *p, int root, int degree, int row){
     int (*array)[degree] = p;
     int remainder = 0;
